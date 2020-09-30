fc17c02e17840fe1ed0918973154b22007ec0977
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2020-09-30 17:39:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function flip(){
 	if (document.getElementById('menu').style.display == 'none'){
 		document.getElementById('menu').style.display = 'block';
+		document.getElementById('menuBTN').innerHTML = '∧';
 	} else {
 		document.getElementById('menu').style.display = 'none';
+		document.getElementById('menuBTN').innerHTML = '∨';
 	}
 }
