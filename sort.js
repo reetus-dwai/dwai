4b97bd5a4272c9b4d56c76282fd3a50b576af40a
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2020-10-02 10:39:25 -0600

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,14 +1,7 @@
 function sort(){
   if (document.getElementById('rarity').value){
     document.getElementById('items').innerHTML = document.getElementById('rarity').value;
-  } 
-  if (document.getElementById('weapons-primary').value){
-    document.getElementById('items').innerHTML = document.getElementById('weapons-primary').value;
-  }
-  if (document.getElementById('weapons-secondary').value){
-    document.getElementById('items').innerHTML = document.getElementById('weapons-secondary').value;
-  }
-  if (document.getElementById('cosmetics').value){
-    document.getElementById('items').innerHTML = document.getElementById('cosmetics').value;
+  } else {
+    document.getElementById('items').innerHTML = 'bruh';
   }
 }
