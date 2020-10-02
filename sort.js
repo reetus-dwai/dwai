d79235df694cceaba048a5dfc1f53a425d1f5c05
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2020-10-02 10:33:33 -0600

    Update sort.js

--- sort.js
+++ sort.js
@@ -11,4 +11,7 @@ function sort(){
   if (document.getElementById('cosmetics').value){
     document.getElementById('items').innerHTML = document.getElementById('cosmetics').value;
   }
+  if (document.getElementById('rarity').value && document.getElementById('weapons-primary' && document.getElementById('weapons-secondary' && document.getElementById('cosmetics'){
+    document.getElementById('items').innerHTML = 'You didn\'t select anything to sort.';
+  }
 }
