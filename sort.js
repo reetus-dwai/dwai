8b2352362b6629b9bd56edefe0f14d90af66d499
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2020-10-02 17:14:14 -0600

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,6 +1,6 @@
 function sort(){
   if (document.getElementById('rarity').value){
-    document.getElementByClass(document.getElementById('rarity').value).style.display = 'block';
+    document.getElementByClassName(document.getElementById('rarity').value).style.display = 'block';
   } else {
     document.getElementById('items').innerHTML = 'You did not choose anything to sort';
   }
