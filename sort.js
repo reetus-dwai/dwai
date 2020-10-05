273ec6b55024ea59277054d3fa001a0fb4b18e3a
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2020-10-04 19:29:59 -0600

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,10 +1,10 @@
 function sort(){
-  document.getElementByTagName('p').style.display = 'block';
+  document.getElementsByTagName('p')[0].style.display = 'block';
   if (document.getElementById('rarity').value){
     for (i = 0; i < document.getElementsByClassName(document.getElementById('rarity').value).length; i++){
     document.getElementsByClassName(document.getElementById('rarity').value)[i].style.display = 'block';
     }
   } else {
-    document.getElementByTagName('p').style.display = 'block';
+    document.getElementsByTagName('p')[0].style.display = 'block';
   }
 }
