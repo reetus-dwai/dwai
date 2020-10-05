42e407cc0a8bfb275533be518eeae9256e1d7fc3
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2020-10-05 08:42:35 -0600

    Update sort.js

--- sort.js
+++ sort.js
@@ -3,8 +3,23 @@ function sort(){
     document.getElementsByTagName('p')[i].style.display = 'none';
   }
   if (document.getElementById('rarity').value){
-    for (i = 0; i < document.getElementsByClassName(document.getElementById('rarity').value).length; i++){
-    document.getElementsByClassName(document.getElementById('rarity').value)[i].style.display = 'block';
+    if (document.getElementById('weapons-primary').value){
+    } else if (document.getElementById('weapons-primary').value){
+      for (i = 0; i < document.getElementsByClassName(document.getElementById('rarity').value).getElementsByClassName(document.getElementById('weapons-primary').value).length; i++){
+        document.getElementsByClassName(document.getElementById('rarity').value).getElementsByClassName(document.getElementById('weapons-primary').value)[i].style.display = 'block';
+      }
+    }  else if (document.getElementById('weapons-secondary').value){
+       for (i = 0; i < document.getElementsByClassName(document.getElementById('rarity').value).getElementsByClassName(document.getElementById('weapons-secondary').value).length; i++){
+        document.getElementsByClassName(document.getElementById('rarity').value).getElementsByClassName(document.getElementById('weapons-secondary').value)[i].style.display = 'block';
+      }
+    }  else if (document.getElementById('cosmetics').value){
+       for (i = 0; i < document.getElementsByClassName(document.getElementById('rarity').value).getElementsByClassName(document.getElementById('cosmetics').value).length; i++){
+        document.getElementsByClassName(document.getElementById('rarity').value).getElementsByClassName(document.getElementById('cosmetics').value)[i].style.display = 'block';
+      }
+    } else {
+      for (i = 0; i < document.getElementsByClassName(document.getElementById('rarity').value).length; i++){
+        document.getElementsByClassName(document.getElementById('rarity').value)[i].style.display = 'block';
+      }
     }
   } else {
       document.getElementsByTagName('p').sort;
